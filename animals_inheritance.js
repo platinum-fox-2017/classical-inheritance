c95907a3383a0006239050dd1ce07e00e4c7c989
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-12 16:27:53 +0700

    Release 1: Super Powered Animals

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,5 +1,14 @@
 "use strict"
 
+class SuperPower {
+  
+  use_laser_vision() {
+    return '========>======>===>>>>>'   
+  }
+  be_invisible(){
+   return '################' 
+  }
+}
 class Animal  {
   constructor(is_warm_blooded)  {
     this.num_legs = 2;
@@ -10,6 +19,7 @@ class Frog extends Animal {
   constructor(is_warm_blooded){
     super(is_warm_blooded)   
     this.num_legs = 4;
+    this.superpower = new SuperPower();
   }
   jump(){
     return 'Kodok Melompat!';
@@ -18,6 +28,7 @@ class Frog extends Animal {
 class Chicken extends Animal {
   constructor(is_warm_blooded){
     super(is_warm_blooded)   
+    this.superpower = new SuperPower();
   }
   berkokok(){
     return 'kukuruyuuuk!';
@@ -27,6 +38,8 @@ class Chicken extends Animal {
 let frog = new Frog(true);
 console.log(frog.jump())
 console.log(frog.num_legs)
+console.log(frog.superpower.use_laser_vision())
 let chicken = new Chicken(false);
 console.log(chicken.berkokok())
 console.log(chicken.num_legs)
+console.log(chicken.superpower.be_invisible());
