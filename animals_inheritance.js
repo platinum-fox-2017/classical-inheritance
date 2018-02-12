cfd96c72ec7e15840f1f6fc3d58fc3da65a128ef
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-12 21:14:44 +0700

    release 1

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -8,36 +8,80 @@ class Animal{
   }
 }
 
+class Superpower{
+  constructor(super_jump,super_speed,super_strong){
+    this.super_jump = super_jump;
+    this.super_speed = super_speed;
+    this.super_strong = super_strong;
+  }
+
+  superJump(){
+    if(this.super_jump){
+      return 'Yes, it has Super Jump!';
+    } else {
+      return 'No, its jump is normal';
+    }
+  }
+
+  superSpeed(){
+    if(this.super_speed){
+      return 'Yes, it has Super Speed!';
+    } else {
+      return 'No, it is so slow :(';
+    }
+  }
+
+  superStrong(){
+    if(this.superStrong){
+      return 'Yes, its Super Strong!';
+    } else {
+      return 'No, it is so weak :(';
+    }
+  }
+
+}
+
 class Rabbit extends Animal{
-  constructor(num_legs,is_warm_blooded,breed_classification){
+  constructor(num_legs,is_warm_blooded,breed_classification,super_jump,super_speed,super_strong){
     super(num_legs,is_warm_blooded,breed_classification);
+    this.superPower = new Superpower(super_jump,super_speed,super_strong);
   }
 }
 
 class Ant extends Animal{
-  constructor(num_legs,is_warm_blooded,breed_classification){
+  constructor(num_legs,is_warm_blooded,breed_classification,super_jump,super_speed,super_strong){
     super(num_legs,is_warm_blooded,breed_classification);
+    this.superPower = new Superpower(super_jump,super_speed,super_strong);
   }
 }
 
 class Bird extends Animal{
-  constructor(num_legs,is_warm_blooded,breed_classification){
+  constructor(num_legs,is_warm_blooded,breed_classification,super_jump,super_speed,super_strong){
     super(num_legs,is_warm_blooded,breed_classification);
+    this.superPower = new Superpower(super_jump,super_speed,super_strong);
   }
 }
 
 class Cat extends Animal{
-  constructor(num_legs,is_warm_blooded,breed_classification){
+  constructor(num_legs,is_warm_blooded,breed_classification,super_jump,super_speed,super_strong){
     super(num_legs,is_warm_blooded,breed_classification);
+    this.superPower = new Superpower(super_jump,super_speed,super_strong);
   }
 }
 
-let rabbit = new Rabbit(2,true,'Vivipar');
-let ant = new Ant(6,true,'Ovipar');
-let bird = new Bird(2,true,'Ovipar');
-let cat = new Cat(4,true,'Vivipar');
+let rabbit = new Rabbit(2,true,'Vivipar',true,true,false);
+let ant = new Ant(6,true,'Ovipar',false,true,true);
+let bird = new Bird(2,true,'Ovipar',false,true,false);
+let cat = new Cat(4,true,'Vivipar',true,true,true);
 
 console.log(rabbit);
+console.log(rabbit.superPower.superJump());
+console.log('--------------------------');
 console.log(ant);
+console.log(ant.superPower.superStrong());
+console.log('--------------------------');
 console.log(bird);
+console.log(bird.superPower.superSpeed());
+console.log('--------------------------');
 console.log(cat);
+console.log(rabbit.superPower.superJump());
