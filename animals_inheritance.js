cdf4db14eb2aa9d64e4cde7270dc494beea7b9a1
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-02-12 14:29:31 +0700

    release 0

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,50 @@
 "use strict"
+class Animal {
+    constructor(name) {
+        this.name = name || 'animal'
+        this.num_legs = 2
+        this.is_warm_blooded = true
+    }
+    numLegs() {
+        console.log(`${this.name} memiliki ${this.num_legs} kaki`);
+    }
+    isWarmBlooded() {
+        console.log(`${this.name} berdarah ${(this.is_warm_blooded ? 'panas' : 'dingin')}`)
+    }
+}
+class Frog extends Animal {
+    constructor() {
+        super('Frog')
+        super.num_legs = 4
+    }
+}
+class Bat extends Animal {
+    constructor() {
+        super('Frog')
+    }
+}
+class Chimpanzee extends Animal {
+    constructor() {
+        super('Chimpanzee')
+    }
+}
+class Fox extends Animal {
+    constructor() {
+        super('Fox')
+        super.num_legs = 4
+        super.is_warm_blooded = false
+    }
+}
+class Chicken extends Animal {
+    constructor() {
+        super('Chicken')
+    }
+}
+let animal = new Animal()
+let frog = new Frog()
+let bat = new Bat()
+let chimpanzee = new Chimpanzee()
+let fox = new Fox()
+let chicken = new Chicken()
+fox.numLegs()
+fox.isWarmBlooded()
