58c7038bf4960aac8d093fd4978d3cdc53e55cbd
Fachrizal Gita <Cecha@Cechas-MacBook-Pro.local>
2018-02-12 14:45:09 +0700

    done

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,53 @@
 "use strict"
+
+class Animal {
+  constructor() {
+    this.num_legs = 4
+    this.is_mammals = true
+    this.can_fly = true
+    this.superpower = new Superpower
+  }
+}
+
+class Chimpanzee extends Animal {
+  constructor() {
+    super()
+    this.num_legs = 2
+    this.can_fly = false
+  }
+}
+
+class Frog extends Animal {
+  constructor() {
+    super()
+    this.is_mammals = false
+    this.can_fly = false
+  }
+}
+
+class Eagle extends Animal {
+  constructor() {
+    super()
+    this.num_legs = 2
+    this.is_mammals = false
+  }
+}
+
+class Superpower {
+  constructor() {
+
+  }
+  use_thunderstorm(){
+    return 'JEDERRRR!!!!'
+  }
+  be_genius(){
+    return `I'm smarter than Stephen Hawking`
+  }
+}
+
+let frog = new Frog
+console.log(frog);
+console.log(frog.superpower.use_thunderstorm());
+let chimpanzee = new Chimpanzee
+console.log(chimpanzee);
+console.log(chimpanzee.superpower.be_genius());
