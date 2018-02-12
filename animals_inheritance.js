671ce797b8444d277e064f30dba9dcf1f5cdd38d
Reza Pramudhika <rezapramudhika@gmail.com>
2018-02-12 16:51:19 +0700

    Release 0

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,50 @@
 "use strict"
+
+class Animal{
+    constructor(num_legs, is_warm_blooded){
+        this.num_legs = num_legs;
+        this.is_warm_blooded = is_warm_blooded;
+    }
+}
+
+class Frog extends Animal{
+    constructor(num_legs, is_warm_blooded){
+        super(num_legs, is_warm_blooded);
+    }
+}
+
+class Bat extends Animal{
+    constructor(num_legs, is_warm_blooded){
+        super(num_legs, is_warm_blooded);
+    }
+}
+
+class Chimpanzee extends Animal{
+    constructor(num_legs, is_warm_blooded){
+        super(num_legs, is_warm_blooded);
+    }
+}
+
+class Fox extends Animal{
+    constructor(num_legs, is_warm_blooded){
+        super(num_legs, is_warm_blooded);
+    }
+}
+
+class Chicken extends Animal{
+    constructor(num_legs, is_warm_blooded){
+        super(num_legs, is_warm_blooded);
+    }
+}
+
+let animal = new Animal(2, true);
+console.log(animal);
+
+let chimpanzee = new Frog(2, true);
+console.log(chimpanzee);
+
+let frog = new Frog(4, false);
+console.log(frog);
+
+let chicken = new Chicken(2, false);
+console.log(chicken);
