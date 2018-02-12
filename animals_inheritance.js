f1f63f31634238ef9ef4110e671f4e444551c677
srohimah <sitirohimach@gmail.com>
2018-02-12 21:36:25 +0700

    super power

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,73 +1,77 @@
 "use strict"
 
 class Animal{
-    constructor(){
+    constructor(){ 
+        this.name = 'Animal'
         this.num_legs = 4
         this.is_warm_blooded = false
-        this.superpower = new Superpower()
+        this.superpower = new Superpower(this.name)
     }
 
 }
 
 class Frog extends Animal{
-    constructor(){
+    constructor(name){
         super()
+        this.name = name
         this.class = 'Amphibians'
     }
 
 }
 
 class Bat extends Animal{
-    constructor(){
+    constructor(name){
         super()
+        this.name = name
     }
 }
 
 class Chimpanzee extends Animal{
-    constructor(){
+    constructor(name){
         super()
+        this.name = name    
         this.num_legs = 2
         this.is_warm_blooded = true
     }
 }
 
 class Fox extends Animal{
-    constructor(){
+    constructor(name){
         super()
+        this.name = name
         this.is_warm_blooded = true
     }
 }
 
 class Chicken extends Animal{
-    constructor(jenis){
-        super()
-        this.class = jenis
+    constructor(name){
+        super()     
+        this.name = name
+        this.num_legs = 2 
+        this.is_warm_blooded = true             
+        this.class = 'birds'
+        this.superpower = new Superpower(name)  
     }
 }
 
 class Superpower{
-    constructor(){
-        this.uselaser_vision = this.use_laser_vision()
-        this.beInvisible = this.be_invisible()
+    constructor(name){
+        this.userLaserVision = this.use_laser_vision(name)
+        this.beInvisible = this.be_invisible(name)
 
     }
-    use_laser_vision(){
-        return 'zzzzzzzzz'
+    use_laser_vision(name){
+        return `zzzzz ${name}`
 
     }
-    be_invisible(){
-        return 'clingg,...'
+    be_invisible(name){
+        return `clingg,...${name}`
     }
 }
 
 let animal = new Animal()
-let frog = new Frog()
-let chimpanzee = new Chimpanzee()
-let fox = new Fox()
-let chicken = new Chicken('birds')
-//console.log(animal)
-// console.log(chimpanzee)
-// console.log(frog)
-// console.log(chicken)
-console.log(fox)
-console.log(fox.superpower.use_laser_vision())
+let frog = new Frog('Tuturu')
+let chicken = new Chicken('kokoro')
+console.log(frog)
+console.log(chicken)
+console.log(chicken.superpower.use_laser_vision('haiyaaa'))
