989f23131f1249fb4e189ad8f170efca3e1ac098
adhiartaputra <adhiartaorabataputra@gmail.com>
2018-02-12 15:04:50 +0700

    Release 0 and 1

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,56 @@
 "use strict"
+class Animal {
+  constructor(legs = 2, hands = 2, food = 'fruit') {
+    this.legs = legs
+    this.hands = hands
+    this.food = food
+    this.ordo = null
+    this.super_power = new SuperPower
+  }
+
+  walk() {
+    return `Walking with ${this.legs} legs`;
+  }
+}
+
+
+class Chimpanzee extends Animal {
+  constructor(name) {
+    super()
+    this.name = name
+    this.ordo = 'Mammals'
+  }
+}
+
+class Chicken extends Animal {
+  constructor(name) {
+    super()
+    this.hands = '2 wings'
+    this.food = 'corn'
+    this.ordo = 'bird'
+    this.name = name
+  }
+}
+
+class SuperPower {
+  constructor() {
+
+  }
+
+  use_fart_bomb() {
+    return `BWOOOSHHH`
+  }
+
+  use_cute_face() {
+    return `CLINGG CLINGG`
+  }
+
+}
+
+
+
+let chimpanzee = new Chimpanzee('Caesar')
+let chicken = new Chicken('Chicken Little')
+// console.log(chimpanzee);
+// console.log(chicken);
+console.log(chimpanzee.super_power.use_fart_bomb());
