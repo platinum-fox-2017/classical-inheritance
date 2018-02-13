806c789e578d3e2427f05a58cb7afdeaf55d3053
FTeddy <ainsophoure@gmail.com>
2018-02-13 11:59:19 +0700

    adjusted class props inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,18 +1,23 @@
 "use strict"
 class Animal {
-  constructor() {
-    this.alive = true
+  constructor(alive) {
+    this.alive = alive
     this.motile = true
     this.num_of_legs = 0
+    this.has_wings = false
+    this.has_gills = false
+    this.has_limbs = false
+    this.vertebrae = true
+    this.warm_blooded = true
+    this.land_creature = true
+    this.sapient = false
   }
 }
 // ******************************
 
 class Mammals extends Animal {
-  constructor() {
-    super()
-    this.warm_blooded = true
-    this.vertebrae = true
+  constructor(alive) {
+    super(alive)
 
   }
 }
@@ -20,9 +25,8 @@ class Mammals extends Animal {
 class Reptiles extends Animal {
   constructor() {
     super()
-    this.cold_blooded = true
-    this.vertebrae = true
-    this.land_creature = true
+    this.warm_blooded = false
+    this.num_of_legs = 4
   }
 }
 
@@ -30,16 +34,15 @@ class Avian extends Animal {
   constructor() {
     super()
     this.has_wings = true
-    this.vertebrae = true
-    this.land_creature =true
+    this.num_of_legs = 2
   }
 }
 
 class Amphibian extends Animal {
   constructor() {
     super()
-    this.has_gills = true
     this.has_limbs = true
+    this.has_gills = true
     this.breathe_underwater = true
   }
 }
@@ -50,6 +53,7 @@ class Fish extends Animal {
     this.has_gills = true
     this.has_fins = true
     this.breathe_undewater = true
+    this.land_creature = false
   }
 }
 
@@ -58,7 +62,7 @@ class Murloc extends Amphibian{
   constructor() {
     super()
     this.can_speak = 'MGGLGLglglglrl!'
-    this.sapient = true
+    this.sapient = false
     this.num_of_legs = 2
   }
 }
@@ -66,8 +70,10 @@ class Murloc extends Amphibian{
 class Bat extends Mammals{
   constructor() {
     super()
+    this.has_wings = true
     this.has_echolocation = true
     this.num_of_legs = 2
+    this.superpower = new SuperPower()
   }
 }
 
@@ -77,7 +83,6 @@ class Chimpanzee extends Mammals{
     this.sapient = true
     this.humanoid = true
     this.num_of_legs = 2
-
   }
 }
 
@@ -92,9 +97,7 @@ class Shark extends Fish{
 class Chicken extends Avian{
   constructor() {
     super()
-    this.num_of_legs = 2
     this.can_fly = false
-
   }
 }
 
@@ -102,18 +105,17 @@ class Komodo extends Reptiles {
   constructor() {
     super()
     this.venomous = true
-    this.num_of_legs = 4
   }
 }
 
 // ******************************************
 
-class SuperPower {
+class SuperPower{
   constructor() {
     this.has_superpowers = true
   }
   use_eye_laser(){
-    console.log('BEEEEAAAAAAMMMMM');
+    console.log(`bat looks at you... BEEEAAAAAAAMMMMMM`);
   }
 
   be_invisible(){
@@ -135,3 +137,6 @@ console.log(chimpanzee);
 console.log(shark);
 console.log(chicken);
 console.log(komodo);
+
+bat.superpower.use_eye_laser()
+bat.superpower.be_invisible()
